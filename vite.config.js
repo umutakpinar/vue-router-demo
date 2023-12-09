3b3852209ada39e7216d7c7df06fed246abf23d0
Umut <umtakpnr5@gmail.com>
2023-12-09 04:29:55 +0300

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias:{
-    '@' : path.resolve(__dirname,'./src')
+  resolve: {
+    alias: {
+      '@' : path.resolve(__dirname, './src')
+    },
   },
   plugins: [vue()],
-})
+});
