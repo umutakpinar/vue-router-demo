0c0dd3b92a742868e9822ab3681f6371e63a52a1
Umut <umtakpnr5@gmail.com>
2023-12-09 04:35:39 +0300

    RouterView & RouterLink added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <!-- Logos -->
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -10,8 +7,15 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <br>
+    <hr>
+    <RouterLink to='/'>Go Home</RouterLink>
+    <hr>
+    <RouterLink to='/about'>Go to About</RouterLink>
+    <hr>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <RouterView>
+  </RouterView>
 </template>
 
 <style scoped>
